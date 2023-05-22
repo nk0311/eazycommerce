da1a3d6995338b086e04f0f5ab278f0abf973725
nk0311 <navaitk031@gmail.com>
2023-05-22 01:58:52 -0400

    fixed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
-import { Container } from 'react-bootstrap'
-
+// import { Container } from 'react-bootstrap'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from  './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { Container } from 'react-bootstrap'
+
+
 
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <Router>
+      <Header />
       <main className="py-3">
-        <Container>
-        <h1>Welcome</h1>
-        </Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path='/product/:id' element={<ProductScreen />} />
+        </Routes>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
