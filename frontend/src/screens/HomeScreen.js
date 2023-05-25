5af1a300dd87b35e17a7cc02b805fa667e5fa1b1
nk0311 <navaitk031@gmail.com>
2023-05-25 19:08:12 -0400

    implemented making API calls, return that data, and setting the state

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import {Row, Col} from 'react-bootstrap'
-// import products from '../products'
 import Product from '../components/Product'
 import axios from 'axios'
 
 function HomeScreen() {
   const [products, setProducts] = useState([])
 
-  useEffect(() => {
+  useEffect(() =>{
 
       async function fetchProducts(){
-        const { data } = await axios.get('https://127.0.0.1:8000/api/products/')
+        const { data } = await axios.get('/api/products/')
         setProducts(data)
       }
       fetchProducts()
