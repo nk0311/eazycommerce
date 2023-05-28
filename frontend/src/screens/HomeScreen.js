996028a927eb8a79298deebda5a70e402d13b87c
nk0311 <navaitk031@gmail.com>
2023-05-28 18:56:34 -0400

    implemented message & loader using bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector} from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 // import axios from 'axios'
 import { listProducts  } from '../actions/productActions'
 
@@ -26,8 +28,8 @@ function HomeScreen() {
   return (
     <div>
         <h1>Latest Products</h1>
-        {loading ? <h2>Loading...</h2>
-            : error ? <h3>{error}</h3>
+        {loading ? <Loader/>
+            : error ? <Message variant='danger'>{error}</Message>
             : 
             <Row>
             {products.map(product => (
