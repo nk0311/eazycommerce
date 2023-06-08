9a6b593148478830b0c26d2e687078c694a17fbb
nk0311 <navaitk031@gmail.com>
2023-06-07 22:05:13 -0400

    implemnted user serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -8,6 +8,10 @@ from . import views
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
+    
+    path('users/profile/', views.getUserProfile, name="user-profile"),
+    
+    
     path('products/', views.getProducts, name="products"),
      path('products/<str:pk>', views.getProduct, name="product"),
 ]
