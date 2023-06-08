f97556df8275980bb857caacaf448c44956d721f
nk0311 <navaitk031@gmail.com>
2023-06-08 00:25:59 -0400

    implemented ability to register user

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -7,7 +7,9 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
+    # path('', views.getRoutes, name="routes"),
+    
+    path('users/register/', views.registerUser, name='register'),
     
     path('users/profile/', views.getUserProfile, name="user-profile"),
     path('users/', views.getUsers, name="users"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -14,6 +14,9 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -30,21 +33,39 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+  
+  
+@api_view(['POST'])    
+def registerUser(request):
+    data = request.data
+    
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>',
-    ]
-    return Response(routes)
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         '/api/products/',
+#         '/api/products/create',
+#         '/api/products/upload/',
+#         '/api/products/<id>/reviews/',
+#         '/api/products/top/',
+#         '/api/products/<id>/',
+#         '/api/products/delete/<id>/',
+#         '/api/products/<update>/<id>',
+#     ]
+#     return Response(routes)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
