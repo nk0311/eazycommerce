d2e41bf380a26b6be7218882de291e6ce92dac81
nk0311 <navaitk031@gmail.com>
2023-06-07 22:42:34 -0400

    implemented protected routes

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -4,40 +4,37 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
 
-
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
     _id = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
-    
+
     class Meta:
         model = User
-        fields = ['id','_id', 'username', 'email', 'name', 'isAdmin']
-        
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
     def get__id(self, obj):
         return obj.id
-    
+
     def get_isAdmin(self, obj):
         return obj.is_staff
-        
+
     def get_name(self, obj):
         name = obj.first_name
         if name == '':
             name = obj.email
-        
-        
         return name
-        
+
+
 class UserSerializerWithToken(UserSerializer):
-    class Meta:
-        token = serializers.SerializerMethodField(read_only=True)
-        model = User
-        fields = ['id','_id', 'username', 'email', 'name', 'isAdmin', 'token']
-        
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta(UserSerializer.Meta):
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
         return str(token.access_token)
-        
 
 
 class ProductSerializer(serializers.ModelSerializer):
