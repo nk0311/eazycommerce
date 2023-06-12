56cc895a4ff777c85c7b3a5b1524bafe4b95f40c
nk0311 <navaitk031@gmail.com>
2023-06-12 11:21:45 -0400

    fixed user update bug

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,23 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col, Table } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
-import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
 
-function ProfileScreen() {
-
+const ProfileScreen = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
 
-  const navigate = useNavigate(); // New hook in React Router 6
-
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const userDetails = useSelector((state) => state.userDetails);
   const { error, loading, user } = userDetails;
@@ -28,32 +27,38 @@ function ProfileScreen() {
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
   const { success } = userUpdateProfile;
 
+
+
   useEffect(() => {
     if (!userInfo) {
-      navigate('/login'); // Use navigate instead of history.push
-    }else{
-        if(!user || !userInfo.name || success){
-            dispatch({type: USER_UPDATE_PROFILE_RESET})
-            dispatch(getUserDetails('profile'))
-        } else {
-            setName(user.name)
-            setEmail(user.email)
-
-        }
+      navigate('/login');
+    } else {
+      if (!user || !user.name || success || userInfo._id !== user._id) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
+        dispatch(getUserDetails('profile'));
+     
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
     }
   }, [dispatch, navigate, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if(password !== confirmPassword) {
-        setMessage('Passwords do not match')
-    } else{
-        dispatch(updateUserProfile({
-            'id':user._id,
-            'name':name,
-            'email':email,
-            'password': password
-        }))
+
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+    } else {
+      dispatch(
+        updateUserProfile({
+          'id': user._id,
+          'name': name,
+          'email': email,
+          'password': password,
+        })
+      );
+      setMessage('');
     }
   };
 
@@ -61,48 +66,49 @@ function ProfileScreen() {
     <Row>
       <Col md={3}>
         <h2>User Profile</h2>
+
         {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
-        <Form.Group controlId='name'>
+          <Form.Group controlId='name'>
             <Form.Label>Name</Form.Label>
             <Form.Control
-                required
-                type='name'
-                placeholder='Enter Name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+              required
+              type='name'
+              placeholder='Enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-        </Form.Group>
+          </Form.Group>
 
-            <Form.Group controlId='email'>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                    required    
-                    type='email'
-                    placeholder='Enter Email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </Form.Group>
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              required
+              type='email'
+              placeholder='Enter Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
 
-            <Form.Group controlId='password'>
+          <Form.Group controlId='password'>
             <Form.Label>Password</Form.Label>
             <Form.Control
-                type='password'
-                placeholder='Enter Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+              type='password'
+              placeholder='Enter Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-            </Form.Group>
+          </Form.Group>
 
-            <Form.Group controlId='passwordConfirm'>
+          <Form.Group controlId='passwordConfirm'>
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
-                type='password'
-                placeholder='Confirm Password'
-                value={confirmPassword}
+              type='password'
+              placeholder='Confirm Password'
+              value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
             />
             </Form.Group>
@@ -121,3 +127,4 @@ function ProfileScreen() {
 }
 
 export default ProfileScreen;
+
