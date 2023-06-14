b76afff197dba2235e9d92e08d2d23884c49c0d2
nk0311 <navaitk031@gmail.com>
2023-06-13 23:52:27 -0400

    implemented ability to create order

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,87 @@
+# from rest_framework import serializers
+# from django.contrib.auth.models import User
+# from rest_framework_simplejwt.tokens import RefreshToken
+# from .models import Product, Order, OrderItem, ShippingAddress
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     name = serializers.SerializerMethodField(read_only=True)
+#     _id = serializers.SerializerMethodField(read_only=True)
+#     isAdmin = serializers.SerializerMethodField(read_only=True)
+
+#     class Meta:
+#         model = User
+#         fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
+
+#     def get__id(self, obj):
+#         return obj.id
+
+#     def get_isAdmin(self, obj):
+#         return obj.is_staff
+
+#     def get_name(self, obj):
+#         name = obj.first_name
+#         if name == '':
+#             name = obj.email
+#         return name
+
+
+# class UserSerializerWithToken(UserSerializer):
+#     token = serializers.SerializerMethodField(read_only=True)
+
+#     class Meta(UserSerializer.Meta):
+#         fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+#     def get_token(self, obj):
+#         token = RefreshToken.for_user(obj)
+#         return str(token.access_token)
+
+
+# class ProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
+        
+# class ShippingAddressSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ShippingAddress
+#         fields = '__all__'
+        
+# class OrderItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = OrderItem
+#         fields = '__all__'
+
+# class OrderSerializer(serializers.ModelSerializer):
+#     OrderItems = serializers.SerializerMethodField(read_only=True)
+#     shippingAddress = serializers.SerializerMethodField(read_only=True)
+#     user = serializers.SerializerMethodField(read_only=True)
+#     class Meta:
+#         model = Order,
+#         fields = '__all__'
+        
+#     def get_orderItems(self, obj):
+#         items = obj.orderItem_set.all()
+#         serializer = OrderItemSerializer(items, many=True)
+#         return serializer.data
+    
+#     def get_shippingAddress(self, obj):
+#         try:
+#             address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+#         except:
+#             address = False
+#         return address
+    
+#     def get_user(self, obj):
+#         user = obj.user
+#         serializer = UserSerializer(user, many=False)
+#         return serializer.data
+
+
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Product, Order, OrderItem, ShippingAddress
+from .models import Product, Order, OrderItem, ShippingAddress, Review
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -23,13 +103,15 @@ class UserSerializer(serializers.ModelSerializer):
         name = obj.first_name
         if name == '':
             name = obj.email
+
         return name
 
 
 class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
 
-    class Meta(UserSerializer.Meta):
+    class Meta:
+        model = User
         fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
 
     def get_token(self, obj):
@@ -37,47 +119,60 @@ class UserSerializerWithToken(UserSerializer):
         return str(token.access_token)
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
-        
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
+
+
 class ShippingAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShippingAddress
         fields = '__all__'
-        
+
+
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
 
+
 class OrderSerializer(serializers.ModelSerializer):
-    orders = serializers.SerializerMethodField(read_only=True)
+    orderItems = serializers.SerializerMethodField(read_only=True)
     shippingAddress = serializers.SerializerMethodField(read_only=True)
     user = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
-        model = Order,
+        model = Order
         fields = '__all__'
-        
-    def get_orders(self, obj):
-        items = obj.orderItem_set.all()
+
+    def get_orderItems(self, obj):
+        items = obj.orderitem_set.all()
         serializer = OrderItemSerializer(items, many=True)
         return serializer.data
-    
+
     def get_shippingAddress(self, obj):
         try:
-            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+            address = ShippingAddressSerializer(
+                obj.shippingaddress, many=False).data
         except:
             address = False
         return address
-    
+
     def get_user(self, obj):
         user = obj.user
         serializer = UserSerializer(user, many=False)
         return serializer.data
-
-
-
-
-
