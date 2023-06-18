0deeec2357dd46c705618dceb262f0c26e4af378
nk0311 <navaitk031@gmail.com>
2023-06-18 13:14:25 -0400

    implemented Admin's ability to delete user

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 
 function UserListScreen() {
@@ -21,9 +21,10 @@ function UserListScreen() {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
-    const deleteHandler = (id) => {
-        
-    }
+    const userDelete = useSelector(state => state.userDelete)
+    const {success : successDelete } = userDelete
+
+ 
 
     useEffect(() => {
         if(userInfo && userInfo.isAdmin){
@@ -32,7 +33,18 @@ function UserListScreen() {
             navigate('/login')
         }
 
-    }, [dispatch, navigate, userInfo])
+    }, [dispatch, navigate, successDelete])
+
+
+    const deleteHandler = (id) => {
+
+        if(window.confirm('Are you sure you want to delete this user?')){
+            dispatch(deleteUser(id))
+        }
+    
+    }
+
+
     return (
         <div>
             <h1>Users</h1>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,8 +3,7 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
-import { userListReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer } from './reducers/userReducers'
 
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer} from './reducers/orderReducers'
 
@@ -17,6 +16,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
+    userDelete: userDeleteReducer,
+
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
