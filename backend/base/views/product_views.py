4c87c7dd3091d86a1023be9c64dd5418ee0f5713
nk0311 <navaitk031@gmail.com>
2023-06-26 22:34:01 -0400

    implemented product image upload

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -71,3 +71,18 @@ def deleteProduct(request, pk):
     product.delete()
     return Response('Product Deleted')
 
+
+
+
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data
+    
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    
+    product.image = request.FILES.get('image')
+    product.save()
+    
+    return Response('Image was uploaded')
+
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,7 +14,13 @@ import {
 
     PRODUCT_CREATE_REQUEST,
     PRODUCT_CREATE_SUCCESS,
-    PRODUCT_CREATE_FAIL
+    PRODUCT_CREATE_FAIL,
+
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+
+
 
 } from '../constants/productConstants'
 
@@ -138,4 +144,49 @@ export const createProduct = () => async (dispatch, getState) => {
         })
     }
 }
+
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+    try{
+        dispatch({
+            type: PRODUCT_UPDATE_REQUEST
+        })
+
+        const {
+            userLogin: {userInfo},
+        }= getState()
+
+        const config = {
+            headers:{
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(
+            `/api/products/update/${product._id}/`,
+            product,
+            config
+        )
+
+        dispatch({
+            type:PRODUCT_UPDATE_SUCCESS,
+            payload: data,
+        })
+
+        dispatch({
+            type: PRODUCT_UPDATE_SUCCESS,
+            payload:data
+        })
+
+    }catch(error){
+        dispatch({
+            type: PRODUCT_UPDATE_FAIL,
+            payload:error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+        })
+    }
+}
+
 
