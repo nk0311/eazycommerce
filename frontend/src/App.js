fc7e3645df508021f03eed5cd590e7957112748e
nk0311 <navaitk031@gmail.com>
2023-06-28 16:43:09 -0400

    implemented Admin order list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
+
 
 
 
@@ -43,6 +45,9 @@ function App() {
 
           <Route path="/admin/productlist" element={<ProductListScreen />} />
           <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+
+
+          <Route path="/admin/orderlist" element={<OrderListScreen />} />
 
         </Routes>
       </main>
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -18,6 +18,10 @@ import {
     ORDER_LIST_MY_SUCCESS,
     ORDER_LIST_MY_FAIL,
     ORDER_LIST_MY_RESET,
+
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
+    ORDER_LIST_FAIL,
     
 } from '../constants/orderConstants'
 
@@ -133,6 +137,32 @@ export const orderListMyReducer = (state={orders:[]}, action) =>{
         case ORDER_LIST_MY_RESET:
             return{
                 orders: []
+            }
+        
+        default:
+            return state
+    }
+
+}
+
+export const orderListReducer = (state={orders:[]}, action) =>{
+    switch(action.type){
+        case ORDER_LIST_REQUEST:
+            return{
+                loading: true,
+            }
+
+        case ORDER_LIST_SUCCESS:
+            return{
+                loading: false,
+                orders: action.payload,
+            }
+
+        
+        case ORDER_LIST_FAIL:
+            return{
+                loading: false,
+                error:action.payload
             }
         
         default:
